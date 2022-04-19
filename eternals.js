1ba2d6bc32416a0132e82742d174df397e3166c1
jared-armstrong <97423883+jared-armstrong@users.noreply.github.com>
2022-04-18 22:03:06 -0400

    updated files

--- eternals.js
+++ eternals.js
@@ -1,55 +1,45 @@
 const eternalsData = [
 
     {
-        horseman: "Black Knight is an expert horseman",
+        name: "Black Knight",
 
-        sword: "Black Knight is an excellent swordsman", 
-
-        weapons: "Black Knight uses the Ebony Blade or Sword of Light and Shield of Night",
+        power: "swordsman", 
 
         pic: "images2\\blackKnight.jpg"
 
     },
 
     {
-        levitate: "Ikaris is able to levitate himself and others at speeds of 850 mph",
+        name: "Ikaris",
 
-        telepathy: "Ikaris is a telepathic individual",
-
-        strength: "Ikaris possesses super human strength!",
+        power: "levitation",
 
         pic: "images2\\ikaris.jpg"
 
     },
 
     {
-        manipulate: "Thena can manipulate cosmic energy to augment her life force",
+        name: "Thena",
 
-        illusions: "Thena can cast illusions and disguise her appearance from others",
-
-        teleport: "Then has the ability to teleport herself and others!",
+        power: "manipulation",
 
         pic: "images2\\thena.jpg"
 
     },
 
     {
-        speed: "Makkari can run at the speed of light",
-
-        weak: "Because Makkari uses their Cosmic Energy towards speed, Makkari has half the strength that normally would be had",
-
-        mechanical: "Makkari is able to pilot most land and air vehicles",
+        name: "Makkari",
+        
+        power: "speed",
 
         pic: "images2\\Makkari_1.jpg"
 
     },
 
     {
-        immunity: "Blade is immune to vampire bites as his mother was bit when he was being born",
+        name: "Blade", 
 
-        healing: "Blade has a rapid healing factor that attacks any alien substances in his body",
-
-        age: "Blade also ages very slowly and can preternaturally sense supernatural activity.",
+        power: "immunity",
 
         pic: "images2\\blade.png"
 
@@ -63,33 +53,35 @@ const showInfo = () => {
 
     let userInput = document.querySelector("#eternalAttribute").value;
     
+    console.log(userInput);
     
     let selectedEternalInfo = document.querySelector(".selectedEternalInfo");
-
-    //let eternal = eternalsData[userInput];
-    // set the information
-
-    selectedEternalInfo = eternalsData.find((element) => element.includes(userInput));
-
-    selectedEternalInfo.textContent = selectedEternalInfo //`${eternal.userInput}`;
     let eternalPic = document.querySelector(".selectedEternalPic");
     // set the image
     eternalPic.src = eternal.pic;
 
-    // JAVASCRIPT
-    // Get a handle to the petsInfo paragraph
-    let eternalsInfo = document.querySelector(".eternalsInfo");
-    // Add content by modifying the textContent property
-    eternalsInfo.textContent = "The Eternals";
+    let eternal = eternalsData.find((eternal) => {
+            if (eternal.power == userInput) {
+                return(eternal);
+            } else 
+                return(null)    
+            }
+    )
+    selectedEternalInfo.textContent = eternal.name + " has the skill of " + userInput;
+};
 
-    let eternalsList = document.querySelector(".eternalsList");
-    let allEternalsList = `<ol>`;
-    eternalsData.forEach((eternal) => {
-        allEternalsList += `<li> ${eternal.name} </li>`;
-    });
-    allPetsList += `</ol>`;
+let eternalsInfo = document.querySelector(".eternalsInfo");
+eternalsInfo.textContent = "The Eternals";
 
-    eternalsList.innerHTML = allEternalsList;
+let allEternalsInfo = `<ul>`;
+eternalsData.forEach((eternal) => {
+    allEternalsInfo += `<li>${eternal.name}</li>`;
+});
+allEternalsInfo += `</ul>`;
 
+allEternalsInfo += `<br> You can choose: `;
+eternalsData.forEach((eternalCharacter) => {
+    allEternalsInfo += eternalCharacter.power + ' ';
+});
 
-}
+eternalsInfo.innerHTML = allEternalsInfo;
